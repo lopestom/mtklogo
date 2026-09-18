ffea175d4e7b58a0216faa8cf2cad308cf1924a0
lopestorm <guillopes2002@gmail.com>
2026-09-18 13:45:06 -0300

    New resolutions & Info
    - Add resolutions  "1220x2712" "1200x1920" "1920x3446" "1080x2340" for more options;
    - New informations about process unpack and repack;
    - New option to attempt to decompress raw files from logo.bin (may be unpack and repack) unknown or new resolutions.

--- mtklogos.sh
+++ mtklogos.sh
@@ -27,32 +27,57 @@ MTKLOGO_DIR=$(dirname "$MTKLOGO")
 cd "$MTKLOGO_DIR" || exit
 echo "Found mtklogo in: $MTKLOGO_DIR"
 
-select action in "Unpack logo.bin" "Repack logo.bin" "Exit"; do
+select action in "Unpack logo.bin" "Repack logo.bin" "Unpack other logo.bin" "Repack other logo.bin" "Exit"; do
     case $action in
         "Unpack logo.bin")
             if [ ! -f "$PWD/logo.bin" ]; then
                 echo "logo.bin not found in current directory."
+                echo -e "\033[1;36m ************************************************* \033[0m "
+                echo -e "\033[5;34m   logo.bin file not found in current directory. \033[0m "
+                echo -e "\033[1;36m ************************************************* \033[0m "
                 exit 1
             fi
             PS1="  What resolution do you device have?  "
-              select resolution in "720x1280" "720x1600" "720x1612" "720x1640" "1080x1920" "1080x2400" "1080x2460" "1280x800"; do
+              select resolution in "720x1280" "720x1600" "720x1612" "720x1640" "1080x1920" "1080x2400" "1080x2460" "1280x800" "1220x2712" "1200x1920" "1920x3446" "1080x2340"; do
               echo "  The choice that you have selected is : $resolution  "
                ./mtklogo unpack -p $resolution logo.bin -o $PWD/logo
-               echo "Unpacking complete."
-               break
-            done
-            ;;
+               echo -e "\033[1;36m *************************** \033[0m "
+               echo -e "\033[5;34m     Unpacking logo_$resolution.bin completed. \033[0m "
+               echo -e "\033[1;36m *************************** \033[0m "
+               ./mtklogos.sh
+        done
+        ;;
+        "Unpack other logo.bin")
+               ./mtklogo unpack logo.bin -o $PWD/logo --mode rgbabe --slots 0
+               echo -e "\033[1;36m *************************** \033[0m "
+               echo -e "\033[5;34m     Unpacking complete. \033[0m "
+               echo -e "\033[1;36m *************************** \033[0m "
+               ./mtklogos.sh
+               ;;
         "Repack logo.bin")
-            ./mtklogo repack -o logo_$resolution.bin $PWD/logo/logo_0*
-            echo "Repacking complete, logo saved in logo_$resolution.bin"
-            break
+         ./mtklogo repack logo/* -o logo_$resolution.bin
+         echo -e "\033[1;36m ********************************************* \033[0m "
+         echo -e "\033[5;34m  Repacking complete, logo saved in logo_repacked.bin \033[0m "
+         echo -e "\033[1;36m ********************************************* \033[0m "
+         ./mtklogos.sh
+        ;;
+        "Repack other logo.bin")
+            cp mtklogo $PWD/logo/
+            sleep 1
+            cd $PWD/logo/
+            ./mtklogo repack --output otherlogo.bin logo_*
+            echo -e "\033[1;36m ************************************************* \033[0m "
+            echo -e "\033[5;34m  Repacking complete, logo saved in otherlogo.bin \033[0m "
+            echo -e "\033[1;36m ************************************************* \033[0m "
+            cd -
             ;;
         "Exit")
             echo "Exiting..."
-            exit 0
+            exit 1
             ;;
         *)
             echo "Invalid option."
             ;;
     esac
 done
+
